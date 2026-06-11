3a4f839c8a9b5f52b333b21ed1f4f3d7de21a7ec
Gabriel <gabrielalveslga1155@gmail.com>
2026-06-11 08:53:46 -0300

    Adicionei o lombok e usei as anotações: @NoArgsConstructor - para facilitar a geração de construtores vazios, @AllArgsConstructor - Facilitar a geração de construtores cheios, e @Data  para criar automaticamente Getters e Setters para deixar o projeto mais simples e legivel

--- src/main/java/dev/studingjava/CadastroDeNinjas/missoes/MissoesModel.java
+++ src/main/java/dev/studingjava/CadastroDeNinjas/missoes/MissoesModel.java
@@ -1,12 +1,18 @@
 package dev.studingjava.CadastroDeNinjas.missoes;
 import dev.studingjava.CadastroDeNinjas.ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name =  "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/studingjava/CadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/dev/studingjava/CadastroDeNinjas/ninjas/NinjaModel.java
@@ -1,23 +1,19 @@
 package dev.studingjava.CadastroDeNinjas.ninjas;
 import dev.studingjava.CadastroDeNinjas.missoes.MissoesModel;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name  = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter
-    @Getter
     private String name;
-    @Setter
-    @Getter
     private String email;
-    @Setter
-    @Getter
     private int age;
 
     // Varios Ninjas podem ter uma missão
@@ -25,15 +21,9 @@ public class NinjaModel {
     @JoinColumn(name =  "missoes_id")  // Foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel(){
-
-    }
-
     public NinjaModel(String name, String email, int age){
         this.name = name;
         this.email = email;
         this.age = age;
     }
-
-
 }
